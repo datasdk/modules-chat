393489a45e65df783768b93646ec8180b84742b9
Kasper <info@datas.dk>
2026-05-17 19:31:15 +0200

    Remove Chat module from repository

--- Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('actions')
-    <a href="{{ route('chat.create') }}" class="btn btn-primary">Opret chat</a>
-@endsection
-
-@section('content')
-    <livewire:table :config="Modules\Chat\Tables\ChatTable::class" />
-@endsection
